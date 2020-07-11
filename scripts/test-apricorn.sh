afbb42d828a864473286d47f8ccbba99a72c98dd
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-11 05:05:47 -0600

    update xmobar

--- scripts/test-apricorn.sh
+++ scripts/test-apricorn.sh
@@ -4,7 +4,7 @@ while :
 do
   if [ -e "/dev/disk/by-uuid/C470-4BC9" ] || [ -e "/dev/disk/by-label/kassir" ]
   then
-    echo "Apricorn mounted"
+    echo "Apricorn found"
   else
     echo "No Apricorn"
   fi
