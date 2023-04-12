0038322a297de50e667c3209978fc80d2ecc4747
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2023-04-12 15:06:47 -0400

    update xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,24 +1,18 @@
 import System.IO
 import XMonad
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Run (spawnPipe)
 
 main = do
   xmproc <- spawnPipe ("xmobar /home/morgan/.xmonad/xmobarrc")
-  xmonad $ defaultConfig
-    { manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts $ layoutHook defaultConfig
-    , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
-    , logHook = dynamicLogWithPP defaultPP
-        { ppOutput = hPutStrLn xmproc
-        , ppVisible = wrap "(" ")"
-        }
+  xmonad $ def
+    { manageHook = manageDocks <+> manageHook def
+    , layoutHook = avoidStruts $ layoutHook def
     , terminal = "tilix"
     , startupHook = startup
     } `additionalKeys`
-    [ ((modMask defaultConfig, xK_Print), spawn "scrot screenshots/screen_%Y-%m-%d-%H-%M-%S.png -d 1")
+    [ ((modMask def, xK_Print), spawn "scrot screenshots/screen_%Y-%m-%d-%H-%M-%S.png -d 1")
     ]
 
 
