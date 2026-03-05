1e089e810520bdf2d7bb0f054dbdcacf998cff93
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2026-03-05 20:35:42 +0000

    delete nixos stuff

--- scripts/show-ssh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-while :
-do
-  ssh-add -l | sed -e 's/256 .*/SSH key loaded/'
-  sleep 1
-done
--- scripts/show-wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-while :
-do
-  FOO=$(nmcli connection show --active | grep wifi | sed -e 's/ .*//g')
-  [ $FOO ] && echo $FOO
-  [ $FOO ] || echo "No Wifi"
-  sleep 1
-done
